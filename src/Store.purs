79392c02f97a8ee71bc4283cf6557b7a7c7e4d2c
bouzuya <m@bouzuya.net>
2019-06-09 14:51:03 +0900

    add Store.put

--- src/Store.purs
+++ src/Store.purs
@@ -2,6 +2,7 @@ module Store
   ( Store
   , get
   , new
+  , put
   ) where
 
 import Prelude
@@ -14,7 +15,10 @@ import Effect.Ref as Ref
 newtype Store a = Store (Ref a)
 
 get :: forall a. Store a -> Aff a
-get (Store ref)= Class.liftEffect (Ref.read ref)
+get (Store ref) = Class.liftEffect (Ref.read ref)
 
 new :: forall a. a -> Aff (Store a)
 new x = Class.liftEffect (map Store (Ref.new x))
+
+put :: forall a. a -> Store a -> Aff Unit
+put x (Store ref) = Class.liftEffect (Ref.write x ref)
