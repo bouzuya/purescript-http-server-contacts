b7cecdb7bcb2a8f005b9659ec7442673e5072665
bouzuya <m@bouzuya.net>
2019-06-15 22:58:10 +0900

    support HTTP 405

--- src/Action.purs
+++ src/Action.purs
@@ -28,25 +28,41 @@ execute store { method, pathname, body } = do
         StatusCode.status301
         [ Tuple.Tuple "Location" (NormalizedPath.toString normalized) ]
     else
-      case method, (NormalizedPath.toPieces normalized) of
-        Method.GET, ["contacts"] -> do
-          contacts <- Store.get store
-          ResponseHelper.json (SimpleJSON.writeJSON contacts)
-        Method.POST, ["contacts"] -> do
-          case (SimpleJSON.readJSON_ body :: _ Contact) of
-            Maybe.Nothing ->
-              ResponseHelper.json'
-                StatusCode.status400
-                (SimpleJSON.writeJSON { message: "bad request" })
-            Maybe.Just contact -> do
+      case NormalizedPath.toPieces normalized of
+        ["contacts"] ->
+          case method of
+            Method.GET -> do
               contacts <- Store.get store
-              let contacts' = Array.insert contact contacts
-              _ <- Store.put contacts' store
               ResponseHelper.json (SimpleJSON.writeJSON contacts)
-        Method.GET, [] ->
-          -- healthcheck
-          ResponseHelper.json (SimpleJSON.writeJSON { message: "OK" })
-        _, _ ->
-          ResponseHelper.json'
-            StatusCode.status404
-            (SimpleJSON.writeJSON { message: "Not Found" })
+            Method.POST -> do
+              case (SimpleJSON.readJSON_ body :: _ Contact) of
+                Maybe.Nothing ->
+                  ResponseHelper.json'
+                    StatusCode.status400
+                    (SimpleJSON.writeJSON { message: "bad request" })
+                Maybe.Just contact -> do
+                  contacts <- Store.get store
+                  let contacts' = Array.insert contact contacts
+                  _ <- Store.put contacts' store
+                  ResponseHelper.json (SimpleJSON.writeJSON contacts)
+            _ ->
+              ResponseHelper.sendStatus
+                StatusCode.status405
+                [ Tuple.Tuple
+                  "Allow"
+                  (String.joinWith ", " (map show [Method.GET, Method.POST]))
+                ]
+        [] ->
+          case method of
+            Method.GET ->
+              -- healthcheck
+              ResponseHelper.json (SimpleJSON.writeJSON { message: "OK" })
+            _ ->
+              ResponseHelper.sendStatus
+                StatusCode.status405
+                [ Tuple.Tuple
+                  "Allow"
+                  (String.joinWith ", " (map show [Method.GET]))
+                ]
+        _ ->
+          ResponseHelper.sendStatus StatusCode.status404 []
