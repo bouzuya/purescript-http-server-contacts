d462360be3ecbf55880a70911e7f7d8e236e414c
bouzuya <m@bouzuya.net>
2019-06-16 11:29:13 +0900

    extract routing middleware

--- src/Action.purs
+++ src/Action.purs
@@ -2,52 +2,22 @@ module Action
   ( execute
   ) where
 
-import Prelude
-
-import Bouzuya.HTTP.Method as Method
 import Bouzuya.HTTP.StatusCode as StatusCode
-import Data.Array as Array
-import Data.Maybe as Maybe
 import Middleware.Logging as MiddlewareLogging
 import Middleware.PathNormalize as MiddlewarePathNormalize
-import NormalizedPath as NormalizedPath
+import Middleware.Routing as MiddlewareRouting
 import ResponseHelper as ResponseHelper
-import Simple.JSON as SimpleJSON
-import Store as Store
-import Type (AppStore, Contact, NewHandler)
+import Type (AppStore, NewHandler)
 
 type R1 r = (store :: AppStore | r)
 type R2 r = MiddlewarePathNormalize.R r
 
 execute :: NewHandler (R1 ())
 execute =
-  (MiddlewareLogging.middleware
-    <<< MiddlewarePathNormalize.middleware)
-  execute'
+  MiddlewarePathNormalize.middleware
+    (MiddlewareLogging.middleware
+      (MiddlewareRouting.middleware
+        handler))
 
-execute' :: NewHandler (R2 (R1 ()))
-execute' { normalizedPath: normalized, request: { method, pathname, body }, store }  = do
-  case NormalizedPath.toPieces normalized of
-    ["contacts"] ->
-      case method of
-        Method.GET -> do
-          contacts <- Store.get store
-          ResponseHelper.fromJSON contacts
-        Method.POST -> do
-          case (SimpleJSON.readJSON_ body :: _ Contact) of
-            Maybe.Nothing ->
-              -- TODO: message
-              ResponseHelper.fromStatus StatusCode.status400 []
-            Maybe.Just contact -> do
-              contacts <- Store.get store
-              let contacts' = Array.insert contact contacts
-              _ <- Store.put contacts' store
-              ResponseHelper.fromJSON contacts
-        _ -> ResponseHelper.status405 [Method.GET, Method.POST]
-    [] ->
-      case method of
-        Method.GET ->
-          -- healthcheck
-          ResponseHelper.fromJSON { message: "OK" }
-        _ -> ResponseHelper.status405 [Method.GET]
-    _ -> ResponseHelper.status404
+handler :: forall r. NewHandler r
+handler _ = ResponseHelper.fromStatus StatusCode.status500 []
---
+++ src/Middleware/Routing.purs
@@ -0,0 +1,49 @@
+module Middleware.Routing
+  ( middleware
+  ) where
+
+import Prelude
+
+import Bouzuya.HTTP.Method as Method
+import Bouzuya.HTTP.StatusCode as StatusCode
+import Data.Array as Array
+import Data.Maybe as Maybe
+import Middleware.PathNormalize as MiddlewarePathNormalize
+import NormalizedPath as NormalizedPath
+import ResponseHelper as ResponseHelper
+import Simple.JSON as SimpleJSON
+import Store as Store
+import Type (Contact, Middleware, AppStore)
+
+type R1 r = (store :: AppStore | r)
+type R2 r = MiddlewarePathNormalize.R r
+
+middleware :: forall r r'. Middleware (R2 (R1 r)) r'
+middleware
+  _
+  { normalizedPath
+  , request: { method, pathname, body }
+  , store
+  } = do
+  case NormalizedPath.toPieces normalizedPath of
+    ["contacts"] ->
+      case method of
+        Method.GET -> do
+          contacts <- Store.get store
+          ResponseHelper.fromJSON contacts
+        Method.POST -> do
+          case (SimpleJSON.readJSON_ body :: _ Contact) of
+            Maybe.Nothing ->
+              -- TODO: message
+              ResponseHelper.fromStatus StatusCode.status400 []
+            Maybe.Just contact -> do
+              contacts <- Store.get store
+              let contacts' = Array.insert contact contacts
+              _ <- Store.put contacts' store
+              ResponseHelper.fromJSON contacts
+        _ -> ResponseHelper.status405 [Method.GET, Method.POST]
+    [] ->
+      case method of
+        Method.GET -> ResponseHelper.fromStatus StatusCode.status200 []
+        _ -> ResponseHelper.status405 [Method.GET]
+    _ -> ResponseHelper.status404
