972cd564b6e1d420c5c6c2b99cccbb4997d3244f
bouzuya <m@bouzuya.net>
2019-06-27 23:22:34 +0900

    refactor Routing middleware

--- src/Action.purs
+++ src/Action.purs
@@ -4,23 +4,77 @@ module Action
 
 import Prelude
 
+import Bouzuya.HTTP.Body as Body
+import Bouzuya.HTTP.Method (Method)
+import Bouzuya.HTTP.Method as Method
+import Bouzuya.HTTP.Request.NormalizedPath (NormalizedPath)
+import Bouzuya.HTTP.Request.NormalizedPath as NormalizedPath
 import Bouzuya.HTTP.StatusCode as StatusCode
+import Data.Array as Array
+import Data.Maybe as Maybe
+import Effect.Class as Class
 import Middleware.Logging as MiddlewareLogging
 import Middleware.PathNormalize as MiddlewarePathNormalize
 import Middleware.Routing as MiddlewareRouting
 import ResponseHelper as ResponseHelper
-import Type (AppStore, NewHandler, Middleware)
+import Simple.JSON as SimpleJSON
+import Store as Store
+import Type (AppStore, Middleware, NewHandler, Contact)
 
 type R1 r = (store :: AppStore | r)
+type R2 r = MiddlewareRouting.R Action r
+
+data Action
+  = ContactCreate
+  | ContactList
+  | HealthCheck
+  | MethodNotAllowed (Array Method)
+  | NotFound
 
 execute :: NewHandler (R1 ())
 execute = middleware handler
 
-handler :: forall r. NewHandler r
-handler _ = ResponseHelper.fromStatus StatusCode.status500 []
+routeImpl :: NormalizedPath -> Method -> Action
+routeImpl normalizedPath method =
+  case NormalizedPath.toPieces normalizedPath of
+    ["contacts"] ->
+      case method of
+        Method.GET -> ContactList
+        Method.POST -> ContactCreate
+        _ -> MethodNotAllowed [Method.GET, Method.POST]
+    [] ->
+      case method of
+        Method.GET -> HealthCheck
+        _ -> MethodNotAllowed [Method.GET]
+    _ -> NotFound
 
-middleware :: forall a. Middleware (R1 ()) a
+handler :: NewHandler (R2 (R1 ()))
+handler { action, request: { body }, store } =
+  -- ResponseHelper.fromStatus StatusCode.status500 []
+  case action of
+    ContactCreate -> do
+      body' <- Class.liftEffect (Body.fromArray body) -- TODO
+      case (SimpleJSON.readJSON_ body' :: _ Contact) of
+        Maybe.Nothing ->
+          -- TODO: message
+          ResponseHelper.fromStatus StatusCode.status400 []
+        Maybe.Just contact -> do
+          contacts <- Store.get store
+          let contacts' = Array.insert contact contacts
+          _ <- Store.put contacts' store
+          ResponseHelper.fromJSON contacts
+    ContactList -> do
+      contacts <- Store.get store
+      ResponseHelper.fromJSON contacts
+    HealthCheck ->
+      ResponseHelper.fromStatus StatusCode.status200 []
+    MethodNotAllowed methods ->
+      ResponseHelper.status405 methods
+    NotFound ->
+      ResponseHelper.status404
+
+middleware :: Middleware (R1 ()) (R2 (R1 ()))
 middleware =
-  MiddlewareRouting.middleware
+  (MiddlewareRouting.middleware routeImpl)
   >>> MiddlewareLogging.middleware
   >>> MiddlewarePathNormalize.middleware
--- src/Middleware/Routing.purs
+++ src/Middleware/Routing.purs
@@ -1,73 +1,28 @@
 module Middleware.Routing
-  ( middleware
+  ( R
+  , middleware
   ) where
 
-import Prelude
-
-import Bouzuya.HTTP.Body as Body
 import Bouzuya.HTTP.Method (Method)
-import Bouzuya.HTTP.Method as Method
 import Bouzuya.HTTP.Request.NormalizedPath (NormalizedPath)
-import Bouzuya.HTTP.Request.NormalizedPath as NormalizedPath
-import Bouzuya.HTTP.StatusCode as StatusCode
-import Data.Array as Array
-import Data.Maybe as Maybe
-import Effect.Class as Class
+import Data.Symbol (SProxy)
+import Data.Symbol as Symbol
 import Middleware.PathNormalize as MiddlewarePathNormalize
-import ResponseHelper as ResponseHelper
-import Simple.JSON as SimpleJSON
-import Store as Store
-import Type (Contact, Middleware, AppStore)
+import Prim.Row (class Lacks)
+import Record as Record
+import Type (Middleware)
 
-type R1 r = (store :: AppStore | r)
-type R2 r = MiddlewarePathNormalize.R r
+type R a r = MiddlewarePathNormalize.R (action :: a | r)
 
-data Action
-  = ContactCreate
-  | ContactList
-  | HealthCheck
-  | MethodNotAllowed (Array Method)
-  | NotFound
+middleware ::
+  forall a r.
+  Lacks "action" r
+  => Lacks "normalizedPath" r
+  => (NormalizedPath -> Method -> a)
+  -> Middleware (MiddlewarePathNormalize.R r) (R a r)
+middleware route next context@{ normalizedPath, request: { method } } = do
+  let action = route normalizedPath method
+  next (Record.insert symbol action context)
 
-route :: NormalizedPath -> Method -> Action
-route normalizedPath method =
-  case NormalizedPath.toPieces normalizedPath of
-    ["contacts"] ->
-      case method of
-        Method.GET -> ContactList
-        Method.POST -> ContactCreate
-        _ -> MethodNotAllowed [Method.GET, Method.POST]
-    [] ->
-      case method of
-        Method.GET -> HealthCheck
-        _ -> MethodNotAllowed [Method.GET]
-    _ -> NotFound
-
-middleware :: forall r r'. Middleware (R2 (R1 r)) r'
-middleware
-  _
-  { normalizedPath
-  , request: { method, body }
-  , store
-  } =
-    case route normalizedPath method of
-      ContactCreate -> do
-        body' <- Class.liftEffect (Body.fromArray body) -- TODO
-        case (SimpleJSON.readJSON_ body' :: _ Contact) of
-          Maybe.Nothing ->
-            -- TODO: message
-            ResponseHelper.fromStatus StatusCode.status400 []
-          Maybe.Just contact -> do
-            contacts <- Store.get store
-            let contacts' = Array.insert contact contacts
-            _ <- Store.put contacts' store
-            ResponseHelper.fromJSON contacts
-      ContactList -> do
-        contacts <- Store.get store
-        ResponseHelper.fromJSON contacts
-      HealthCheck ->
-        ResponseHelper.fromStatus StatusCode.status200 []
-      MethodNotAllowed methods ->
-        ResponseHelper.status405 methods
-      NotFound ->
-        ResponseHelper.status404
+symbol :: SProxy "action"
+symbol = Symbol.SProxy
